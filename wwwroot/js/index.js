29a273dec746ce1e715b325238a3afd04a801d31
Md Jamil Uddin <azad1031@gmail.com>
2020-08-11 03:55:25 +0200

    HTML and CSS

---
+++ wwwroot/js/index.js
@@ -0,0 +1,28 @@
+﻿
+$(document).ready(function () {
+    var x = 0;
+    var s = "";
+    console.log("Hello PluralSight");
+
+
+    var theForm = $("#theForm");
+    theForm.hide();
+
+    var button = $("#buyButton");
+    button.on("click", function () {
+        console.log("Buying Item");
+    });
+
+    var productInfo = $(".product-info li");
+    productInfo.on("click", function () {
+        console.log(" You are Clicked on" + $(this).text());
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.toggle(1000);
+    });
+
+});
